773b13dd60ff98112ba074839745dab51558e670
timothy1205 <timothy120500@gmail.com>
2022-03-18 14:06:44 -0600

    Added LED functions and blink functionality

--- project/output.c
+++ project/output.c
@@ -26,8 +26,81 @@ void buzzer_set_period(short cycles)
   CCR1 = cycles >> 1;		/* one half cycle */
 }
 
+static LedState led_green = LED_OFF;
+static LedState led_red = LED_OFF;
+static unsigned char led_counter = 0;
+static unsigned char led_blink_on = 1;
+
 void led_init(void)
 {
+  led_green = LED_OFF;
+  led_red = LED_OFF;
   P1DIR |= LEDS;
   P1OUT &= ~LEDS;		/* leds initially off */
+}
+
+void led_green_on(void)
+{
+  led_green = LED_ON;
+  P1OUT |= LED_GREEN;
+}
+
+void led_green_off(void)
+{
+  led_green = LED_OFF;
+  P1OUT &= ~LED_GREEN;
+}
+
+void led_green_blink(void)
+{
+  if (led_green == LED_BLINK)
+    led_red_off();
+
+  led_green = LED_BLINK;
+  led_counter = 0;
+  led_blink_on = 1;
+  P1OUT |= LED_GREEN;
+}
+
+void led_red_on(void)
+{
+  led_red = LED_ON;
+  P1OUT |= LED_GREEN;
+}
+
+void led_red_off(void)
+{
+  led_red = LED_OFF;
+  P1OUT &= ~LED_RED;
+}
+
+void led_void_blink(void)
+{
+  if (led_green == LED_BLINK)
+    led_green_off();
+
+  led_red = LED_BLINK;
+  led_counter = 0;
+  led_blink_on = 1;
+  P1OUT |= LED_RED;
+}
+
+inline static void led_blink_toggle(void)
+{
+  led_counter = 0;
+  led_blink_on = !led_blink_on;
+  P1OUT ^= (led_green == LED_BLINK ? LED_GREEN : LED_RED);
+}
+
+void led_timer_interrupt(void)
+{
+  // If no LEDS are blinking exit early
+  if (led_green != LED_BLINK && LED_RED != LED_BLINK)
+    return;
+
+  // At most only one LED will ever be blinking
+  if (led_blink_on && led_counter++ >= LED_ON_INTERRUPTS)
+    led_blink_toggle();
+  else if (!led_blink_on && led_counter++ >= LED_OFF_INTERRUPTS)
+    led_blink_toggle();
 }
--- project/output.h
+++ project/output.h
@@ -9,6 +9,15 @@
 void buzzer_init(void);
 void buzzer_set_period(short cycles);
 
+#define LED_ON_INTERRUPTS 250
+#define LED_OFF_INTERRUPTS 250
+typedef enum LedState{ LED_ON, LED_OFF, LED_BLINK } LedState;
 void led_init(void);
+void led_green_on(void);
+void led_green_off(void);
+void led_green_blink(void);
+void led_red_on(void);
+void led_red_off(void);
+void led_red_blink(void);
 
 #endif // included
